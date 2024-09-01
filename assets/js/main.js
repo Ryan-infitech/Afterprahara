34ce16a939a2cb415a5ebc9d9e860cccf430b0d1
Rian Septiawan <rianseptiawan@infitech.or.id>
2024-09-01 08:47:38 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,221 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('is-touch');
+
+	// Forms.
+		var $form = $('form');
+
+		// Auto-resizing textareas.
+			$form.find('textarea').each(function() {
+
+				var $this = $(this),
+					$wrapper = $('<div class="textarea-wrapper"></div>'),
+					$submits = $this.find('input[type="submit"]');
+
+				$this
+					.wrap($wrapper)
+					.attr('rows', 1)
+					.css('overflow', 'hidden')
+					.css('resize', 'none')
+					.on('keydown', function(event) {
+
+						if (event.keyCode == 13
+						&&	event.ctrlKey) {
+
+							event.preventDefault();
+							event.stopPropagation();
+
+							$(this).blur();
+
+						}
+
+					})
+					.on('blur focus', function() {
+						$this.val($.trim($this.val()));
+					})
+					.on('input blur focus --init', function() {
+
+						$wrapper
+							.css('height', $this.height());
+
+						$this
+							.css('height', 'auto')
+							.css('height', $this.prop('scrollHeight') + 'px');
+
+					})
+					.on('keyup', function(event) {
+
+						if (event.keyCode == 9)
+							$this
+								.select();
+
+					})
+					.triggerHandler('--init');
+
+				// Fix.
+					if (browser.name == 'ie'
+					||	browser.mobile)
+						$this
+							.css('max-height', '10em')
+							.css('overflow-y', 'auto');
+
+			});
+
+	// Menu.
+		var $menu = $('#menu');
+
+		$menu.wrapInner('<div class="inner"></div>');
+
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+
+				var href = $(this).attr('href');
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Hide.
+					$menu._hide();
+
+				// Redirect.
+					if (href == '#menu')
+						return;
+
+					window.setTimeout(function() {
+						window.location.href = href;
+					}, 350);
+
+			})
+			.append('<a class="close" href="#menu">Close</a>');
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('click', function(event) {
+
+				// Hide.
+					$menu._hide();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+document.body.classList.toggle('dark-mode');
+});
+					
+
+			});
+//Darkmode
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+	document.body.classList.toggle('dark-mode');
+  
+	// Menyimpan preferensi pengguna di localStorage
+	if (document.body.classList.contains('dark-mode')) {
+	  localStorage.setItem('darkMode', 'enabled');
+	} else {
+	  localStorage.setItem('darkMode', 'disabled');
+	}
+  });
+  
+  // Mengecek preferensi pengguna dari localStorage
+  if (localStorage.getItem('darkMode') === 'enabled') {
+	document.body.classList.add('dark-mode');
+  }
+//Darkmode
+
+//music
+  document.addEventListener('DOMContentLoaded', function() {
+	var audio = document.getElementById('myAudio');
+	var musicControl = document.getElementById('musicControl');
+  
+	musicControl.addEventListener('click', function() {
+	  if (audio.paused) {
+		audio.play();
+		musicControl.classList.remove('fa-play');
+		musicControl.classList.add('fa-pause');
+	  } else {
+		audio.pause();
+		musicControl.classList.remove('fa-pause');
+		musicControl.classList.add('fa-play');
+	  }
+	});
+  });
+//music
+
+  
+})(jQuery);
